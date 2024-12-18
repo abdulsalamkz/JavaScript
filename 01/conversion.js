acba2e3f88f5909deecbaa04764876b06ac877cd
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2024-12-18 16:23:31 +0500

    push

---
+++ 01/conversion.js
@@ -0,0 +1,17 @@
+let score = "0";
+console.log(typeof score);
+console.log(score);
+
+let num = Number(score)
+let num1 = Boolean(score)
+console.log(typeof num);
+console.log(num);
+
+console.log(typeof num1);
+console.log(num1);
+
+// "33" = 33
+// "33abc" = NaN
+// "33" 
+// "abc" = true
+// "" = false
