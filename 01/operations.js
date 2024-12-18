3629344bd12a84ae4f5d35c06d37b1ab7447f09c
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2024-12-18 17:29:21 +0500

    operations

---
+++ 01/operations.js
@@ -0,0 +1,10 @@
+console.log(1 + 3);
+console.log("1" + 3);
+console.log(2 + 3 + "2");
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " World"
+console.log(str1 + str2);
