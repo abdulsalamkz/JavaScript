90d2d333a4996578e7330c745650c9df9b26b0e2
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2024-12-19 16:01:34 +0500

    comparison

---
+++ 01/comparison.js
@@ -0,0 +1,9 @@
+console.log(4>3);
+console.log(4<3);
+console.log(4 == "4");
+console.log(4 === "4");
+console.log(4 === 4);
+
+console.log( null >= 0);
+console.log( null > 0);
+console.log( null < 0);
