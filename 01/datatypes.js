c58b22ed0849a242d985bfdb69f2abbcfe214605
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2024-12-19 17:18:49 +0500

    datatypes

--- 01/datatypes.js
+++ 01/datatypes.js
@@ -4,11 +4,39 @@ let name = "AbdulSalam";
 let age = 21;
 let isLoggedIn = false;
 
-
 // Data Type
-// number 
+// number
 // string
 // boolean
 // null
 // undefined
 // symbol
+
+// // // // // Primitive // // // // //
+
+// 1: String
+// 2: Number
+// 3: Boolean
+// 4: Null
+// 5: Undefined
+// 6: Symbol
+// 7: Bigint
+
+// // // // // Non-primitive // // // // //
+
+// 1: Array
+// 2: Object
+// 3: Function
+
+let arr = ["Hamza", "Shazeel", "AbdulSalam"];
+let obj = {
+  name: "AbdulSalam",
+  age: 21,
+  roll_no: 1,
+};
+function func() {
+  console.log("Hello world");
+}
+console.log(arr);
+console.log(obj);
+func()
