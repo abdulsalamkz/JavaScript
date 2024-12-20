417ecb7453202e864cf3a2bdc8a6802fa5aff1c1
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2024-12-20 14:30:49 +0500

    strings

---
+++ 01/strings.js
@@ -0,0 +1,15 @@
+let name = "Abdulsalam"
+let repo = 8
+
+console.log(name + repo );
+console.log(`my name is ${name} and my repo count is ${repo}`);
+
+let gameName = new String("Batman Arkam Knight")
+console.log(gameName);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf("m"));
+console.log(gameName.charAt(5));
+console.log("   a b  tt  dd  ".trim());
+
+
+console.log(gameName.split(" "))
